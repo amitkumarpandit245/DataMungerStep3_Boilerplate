adad24b0a4e1d5dfa542c5a93adbba1f057ff0cf
Ubuntu <ubuntu@ip-172-31-41-20.ec2.internal>
2019-11-08 04:06:29 +0000

    DataMunger03

--- src/com/stackroute/datamunger/query/Header.java
+++ src/com/stackroute/datamunger/query/Header.java
@@ -6,9 +6,17 @@ public class Header {
 	 * This class should contain a member variable which is a String array, to hold
 	 * the headers.
 	 */
-	
+	String[] header;
+	public Header(String[] header) {
+		this.header = header;
+	}
+
 	public String[] getHeaders() {
-		return null;
+
+		return header;
+	}
+	public void setHeaders(String[] header) {
+		this.header = header;
 	}
 
 }
